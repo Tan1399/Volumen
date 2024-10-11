ea7364dbc6baebed8b873009878fb573a215c983
Sebastian <snvillalobos64@gmail.com>
2024-10-11 15:41:44 -0600

    Agregar archivos de proyecto.

---
+++ Volumen/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Volumen
+{
+    partial class Volumen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Volumen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.volumen_control1 = new Volumen.volumen_control();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-6, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VOLUMEN: 30%";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, -10);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(502, 207);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // volumen_control1
+            // 
+            this.volumen_control1.BackColor = System.Drawing.Color.Black;
+            this.volumen_control1.Bar_color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.volumen_control1.Location = new System.Drawing.Point(-3, 236);
+            this.volumen_control1.Max = 100;
+            this.volumen_control1.Min = 0;
+            this.volumen_control1.Name = "volumen_control1";
+            this.volumen_control1.Size = new System.Drawing.Size(470, 30);
+            this.volumen_control1.TabIndex = 0;
+            this.volumen_control1.Value = 50;
+            this.volumen_control1.Load += new System.EventHandler(this.volumen_control1_Load);
+            this.volumen_control1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.volumen_control1_MouseMove);
+            // 
+            // Volumen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 261);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.volumen_control1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Volumen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Volumen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private volumen_control volumen_control1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Volumen/volumen_control.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volumen
+{
+    public partial class volumen_control : UserControl
+    {
+        public volumen_control()
+        {
+            InitializeComponent();
+            this.Size = new Size(350,30);
+            this.BackColor = Color.Black;
+            DoubleBuffered = true;
+        }
+        int pb_value = 40, pb_Min = 0, pb_Max = 100;
+        public int Max { get { return pb_Max; } set { pb_Max = value; Invalidate(); } }
+        public int Min { get { return pb_Min; } set { pb_Min = value; Invalidate(); } }
+
+        public int Value { get { return pb_value; } set { pb_value = value; Invalidate(); } }
+
+        private void volumen_control_Load(object sender, EventArgs e)
+        {
+             
+        }
+        Color b_color = Color.Aqua;
+        public Color Bar_color { get { return b_color; } set { b_color = value;Invalidate(); } }
+
+        public int gap = 10;
+
+        private void volumen_control_Paint(object sender, PaintEventArgs e)
+        {
+            int start_point = 40;
+            SolidBrush sb = new SolidBrush(Color.DimGray);
+            for (int j = 0; j < Max*ClientSize.Width/ Max - 75 / gap; j++)
+            {
+                e.Graphics.FillRectangle(sb, new Rectangle(start_point, 0, gap - 5, ClientSize.Height));
+                start_point += gap;
+            }
+            int buffer_point = 40;
+            SolidBrush br = new SolidBrush(b_color);
+            for( int i=0; i < (pb_value * ClientSize.Width / Max - pb_value) / gap; i++)
+            {
+                e.Graphics.FillRectangle(br, new Rectangle(buffer_point, 0, gap - 2, ClientSize.Height));
+                buffer_point += gap;
+
+            }
+            int thum_size = 25;
+            SolidBrush thumb = new SolidBrush(Color.White);
+            e.Graphics.FillRectangle(thumb, new Rectangle(buffer_point, 0, thum_size, ClientSize.Height));
+
+
+        }
+        private void Bar_value(float value)
+        {
+            if (value < Min) value = Min;
+            if (value > Max) value = Max;
+            if (pb_value == value) return;
+            pb_value = (int)value;
+            this.Refresh();
+
+
+        }
+        private float thumb_value(int x)
+        {
+            return Min + (Max - Min) * x / (float)(ClientSize.Width);
+        }
+
+       
+
+        bool mouse = false;
+
+       
+        private void volumen_control_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = true;
+            Bar_value(thumb_value(e.X));
+
+        }
+
+        private void volumen_control_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouse) return;
+            Bar_value(thumb_value(e.X));
+
+
+        }
+
+        private void volumen_control_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = false;
+        }
+
+
+
+
+
+
+    }
+}
